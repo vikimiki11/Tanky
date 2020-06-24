57d4a1a087401db044d40e02dca75d7a1ff7f86b
vikimiki11 <34626149+vikimiki11@users.noreply.github.com>
2020-06-24 15:07:58 +0200

    střelba ničení terénu spawnování prostě všechno funguje ještě repearnu ježdění, protože v dírách je to divný, ale jinak přecházím z test fáze do reálné produkce

--- testování/js.js
+++ testování/js.js
@@ -26,8 +26,6 @@ function init() {
   canvas = document.getElementById('Canvas');
   context = canvas.getContext('2d');
   context.imageSmoothingEnabled = false
-
-  // Set the canvas the same width and height of the image
   canvas.width = mapax;
   canvas.height = mapay;
   document.getElementById('Generace').addEventListener('click', function() { createterain(Math.random() * 99) });
@@ -52,6 +50,9 @@ function init() {
     if (event.keyCode == 37 || event.keyCode == 38 || event.keyCode == 39 || event.keyCode == 40) {
       clearInterval(pohyb)
       pohyb=""
+    }
+    if (event.keyCode == 32){
+      fire(tanky[0], "viki", 8)
     }
   })
   createterain(Math.random() * 99);
@@ -97,6 +98,7 @@ function spawntank(x, owner) {
   tanky[pos].x = x
   tanky[pos].y = 0
   tanky[pos].aim = 0
+  tanky[pos].rotate = 0
   tanky[pos].own = owner
   document.querySelector(".tanky").innerHTML += '<div class="tank" id="' + owner + '" style="left:' + x + 'px;top:0px"><div class="cannon"></div></div>'
   tanky[pos].gravity = gravity(0, tanky[pos])
@@ -106,7 +108,9 @@ function spawntank(x, owner) {
 function rtd(rad) {
   return rad * (180 / Math.PI)
 }
-
+function dtr(deg) {
+  return Math.PI / 180 * deg
+}
 function gravity(jump, ob) {
   if (mapa[Math.round(ob.x)][Math.round(ob.y)] == false) {
     ob.y += jump
@@ -114,11 +118,6 @@ function gravity(jump, ob) {
     document.querySelector("#" + ob.own).style.top = ob.y + "px"
     setTimeout(function() { gravity(jump, ob) }, tick)
   } else {
-    p = 1
-    while (mapa[Math.round(ob.x)][Math.round(ob.y - p)] == true) {
-      p++
-    }
-    ob.y = ob.y - p + 2
     document.querySelector("#" + ob.own).style.top = (ob.y) + "px"
     rotation(ob)
   }
@@ -150,29 +149,37 @@ function rotation(ob) {
     posun=90
   }
   if (zmena != 0) {
-    document.querySelector("#" + ob.own).style.transform = "rotate(" + (posun+rtd(Math.atan(50 / zmena))) + "deg)translate(-37.5px, -37.5px)"
+    xsus=posun+rtd(Math.atan(50 / zmena))
+    document.querySelector("#" + ob.own).style.transform = "rotate(" + xsus + "deg)translate(-37.5px, -37.5px)"
+    ob.rotate = xsus
   }
 }
 setTimeout(function() { spawntank(200, "ahoj") }, 1000)
 init()
-viki = ""
 
-function testrotation(ob, r) {
-  viki = setTimeout(function() { testrotation(ob, r + 1) }, 50)
-  document.querySelector("#" + ob.own).style.transform = "rotate(" + r + "deg)translate(-25px, -25px)"
-}
-
-updown=[-4,-3,-2,-1,0,1,2]
+updown=[-2,-1,0,1,2,3,4]
 function brm(smer,ob) {
+  mem=ob.x
   for(i=0;i<updown.length;i++){
     if(mapa[Math.round(ob.x+smer)][Math.round(ob.y+updown[i])]!=mapa[Math.round(ob.x+smer)][Math.round(ob.y+updown[i]-1)]){
-      ob.x+=smer
+      if(updown[i]<0){
+        ob.x+=(smer/(2-updown[i]))
+      }else{
+        ob.x+=smer
+      }
       ob.y+=updown[i]
       document.querySelector("#" + ob.own).style.top = ob.y + "px"
       document.querySelector("#" + ob.own).style.left = ob.x + "px"
     }
   }
-  rotation(ob)
+  if(mem==ob.x && !mapa[Math.round(ob.x+smer)][Math.round(ob.y)]){
+    ob.x+=smer
+    clearInterval(pohyb)
+    pohyb=""
+    gravity(0,ob)
+  }else{
+    rotation(ob)
+  }
 }
 
 function mir(smer, ob) {
@@ -183,29 +190,41 @@ function mir(smer, ob) {
     if (ob.aim > 180) {
       ob.aim = 180
     }}
-  document.querySelector("#" + ob.own+" .cannon").style.transform = "rotate(" + ob.aim + "deg)translate(15.5px,6px)"
+  document.querySelector("#" + ob.own+" .cannon").style.transform = "rotate(" + ob.aim + "deg)translate(10.5px,6px)"
 }
 function fire(ob, typ, speed){
  pos = kulky.length
  kulky[pos] = {}
  kulky[pos].typ = typ
- kulky[pos].x = x
- kulky[pos].y = 0 //kokooooooooooooooooooooooooooooooooooooooooooooooooooooooooot
- document.querySelector(".tanky").innerHTML += '<div class="tank" id="' + typ + '" style="left:' + kulky[pos].x + 'px;top:'+ kulky[pos].y +'px"></div>'
- letim(0,0,kulky[pos])
+ xcan=Math.cos(dtr(ob.rotate+ob.aim-180)*-1)
+ ycan=Math.sin(dtr(ob.rotate+ob.aim-180)*-1)
+ kulky[pos].x = ob.x-Math.sin(dtr(ob.rotate)*-1)*30.5+xcan*27
+ kulky[pos].y = ob.y-Math.cos(dtr(ob.rotate)*-1)*30.5-ycan*27
+ document.querySelector(".tanky").innerHTML += '<div class="kulka" id="' + typ + '" style="left:' + kulky[pos].x + 'px;top:'+ kulky[pos].y +'px"></div>'
+ hore=(xcan*xcan+ycan*ycan)*speed*speed
+ ys=Math.sqrt(hore/((xcan*xcan/(ycan*ycan))+1))
+ xs=Math.sqrt(hore/((ycan*ycan/(xcan*xcan))+1))
+ if(ob.rotate+ob.aim<180&&0<ob.rotate+ob.aim){
+   ys=ys*(-1)
+ }
+ if(ob.rotate+ob.aim<90&&-90<ob.rotate+ob.aim){
+  xs=xs*(-1)
+}
+ letim(xs ,ys ,kulky[pos])
 }
 function letim(speedx,speedy,ob){
   ob.x+=speedx
   ob.y+=speedy
-  document.querySelector("#"+ob.typ).style.top=ob.y
-  document.querySelector("#"+ob.typ).style.left=ob.x
-  speedx=speedx*0.9
-  speedy=speedy*0.9+10/(1000/tick)
-  if(Math.round(ob.x) > -1 && Math.round(ob.x) < mapax + 1 && Math.round(ob.y) > -1 && Math.round(ob.y) < mapay + 1){
+  document.querySelector(".kulka").style.top=ob.y+"px"
+  document.querySelector(".kulka").style.left=ob.x+"px"
+  speedx=speedx*0.99
+  speedy=speedy*0.99+10/(1000/tick)
+  if(Math.round(ob.x) > -1 && Math.round(ob.x) < mapax + 1  && Math.round(ob.y) < mapay + 1){
     if (!mapa[Math.round(ob.x)][Math.round(ob.y)]) {
       setTimeout(function(){letim(speedx,speedy,ob)},tick)
     }else {
       document.querySelector("#" + ob.typ).remove
+      removeter(Math.round(ob.x),Math.round(ob.y),15)
     }
   }else{
     document.querySelector("#"+ob.typ).remove
@@ -215,4 +234,37 @@ function bum(x,y,ob){
  imageData = context.getImageData(0, 0, canvas.width, canvas.height);
  imageData.data
  context.putImageData(imageData, 0, 0); 
+}
+function removeter(xp,yp,r){
+  qd=[]
+  x=0
+  while(x<=r){
+    outof=false
+    y=0
+    while(!outof){
+      if(Math.sqrt(x*x+y*y)>r){
+        outof=true
+      }else{
+        qd[qd.length]=[x,y]
+        y++
+      }
+    }
+    x++
+  }
+  qtf=[-1,1]
+  fd=[]
+  for(i=0;i<qd.length;i++){
+    for(x=0;x<2;x++){
+      for(y=0;y<2;y++){
+        fd[fd.length]=[qd[i][0]*qtf[x],qd[i][1]*qtf[y]]
+      }
+    }
+  }
+  for(i=0;i<fd.length;i++){
+    mapa[xp+fd[i][0]][yp+fd[i][1]]=false
+  }
+  aktualizace()
+  for(i=0;i<tanky.length;i++){
+    gravity(0, tanky[i])
+  }
 }
